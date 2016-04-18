c131160512b789e029763ba7096bf484764093e7
Martim Ferreira Jose <martim.ferreira.jose@gmail.com>
2016-04-18 20:18:14 -0300

    coloquei traço na celula sem nada e mudei a fonte do texto que vai no botao

--- ClassTabuleiro.py
+++ ClassTabuleiro.py
@@ -20,7 +20,7 @@ class tabuleiro:
        for i in range(0,3):
            for j in range(0,3):
                matriz_botoes[i][j] = tk.Button(self.tabuleiro)
-               matriz_botoes[i][j].config(width = 6, height = 3 )
+               matriz_botoes[i][j].config(width = 6, height = 3, text="---", font=("Helvetica", 24))
                matriz_botoes[i][j].grid(row =  i, column = j, sticky = "nsew")    
                  
       
