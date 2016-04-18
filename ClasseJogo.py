3c529ac446929f4db096e1399ed4562dd3831caf
Martim Ferreira Jose <martim.ferreira.jose@gmail.com>
2016-04-18 18:19:25 -0300

    Arquivo com as funçoes basicas para serem iimplementadas

---
+++ ClasseJogo.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 14 15:13:08 2016
+
+@author: Martim
+"""
+
+#receber jogada por botao, varificar se jogo terminou, 
+#alterar jogadores
+#classe jogo não tem acesso a classe tabuleiro
+    
+class jogo:
+
+    def recebe_jogada(self, linha, coluna): #receber e registra a jogada, deve alterar os jogadores e não retorna nada.
+
+    
+    def verifica_ganhador(self): #Retorna 0 em empate, 1 se X vencer, 2 se O vencer, -1 caso contrário.
+     
+
+    def limpa_jogadas(self): #Limpa jogadas (reninia o jogo)
