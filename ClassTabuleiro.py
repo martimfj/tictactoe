bf7d519a588a1e0ac1d09c264d39e21bd2981a59
raphacosta27 <rapha2706@hotmail.com>
2016-04-18 19:09:33 -0300

    Gerações de botão atualizados para méto quase uma matriz

--- ClassTabuleiro.py
+++ ClassTabuleiro.py
@@ -3,23 +3,28 @@ import tkinter as tk
 class tabuleiro:
     def __init__(self):
        self.tabuleiro = tk.Tk()
-       self.tabuleiro.geometry("378x340")
+       self.tabuleiro.geometry("300x300")
        
        #Colunas e fileiras:        
        for i in range (0,3):
-           self.tabuleiro.rowconfigure(i, minsize=0, weight=1)
+           self.tabuleiro.rowconfigure(i, weight=1)
        
        for i in range (0,3):        
-           self.tabuleiro.columnconfigure(i, minsize=0, weight=1)
+           self.tabuleiro.columnconfigure(i, weight=1)
            
        #Botões:
-       for i in range (0,3):
-           for j in range(0,3):    
-                   botão = tk.Button(self.tabuleiro)
-                   botão.config(width = 12, height = 6 )
-                   botão.grid(row =  i, column = j, sticky = "nsew")       
-       
-       
+       matriz_botoes = [[1,2,3],
+                        [4,5,6],
+                        [7,8,9]]
+  
+       for i in range(0,3):
+           for j in range(0,3):
+               matriz_botoes[i][j] = tk.Button(self.tabuleiro)
+               matriz_botoes[i][j].config(width = 6, height = 3 )
+               matriz_botoes[i][j].grid(row =  i, column = j, sticky = "nsew")    
+                 
+      
+      
        label = tk.Label(text='Próxima jogada:')
        label.grid(row=3, column=0, columnspan=2, sticky="nsw")
 
