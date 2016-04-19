d5e67ae9ae46c8e2d96a5ccac49180f1859a413b
Martim Ferreira Jose <martim.ferreira.jose@gmail.com>
2016-04-19 11:48:47 -0300

    modificado a classe jogo, adicionado linhas na função recebe jogada e metodo init

--- ClasseJogo.py
+++ ClasseJogo.py
@@ -5,16 +5,30 @@ Created on Thu Apr 14 15:13:08 2016
 @author: Martim
 """
 
-#receber jogada por botao, varificar se jogo terminou, 
+#receber jogada por botao, verificar se jogo terminou, 
 #alterar jogadores
-#classe jogo não tem acesso a classe tabuleiro
     
-class jogo:
+class Jogo:
+    def __init__(self):
+        self.tabuleiro = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        self.jogador = 1 #1 = Jogador X
 
     def recebe_jogada(self, linha, coluna): #receber e registrar a jogada, deve alterar os jogadores e não retorna nada.
-        
+        self.tabuleiro[linha][coluna] = self.jogador
+        if self.jogador == 1:
+        	self.jogador = 2
+        else: 
+        	self.jogador = 1
     
     def verifica_ganhador(self): #Retorna 0 em empate, 1 se X vencer, 2 se O vencer, -1 caso contrário.
-     
+    	for 
+    	if self.tabuleiro: 		#O venceu
+            return 2 
+        elif  	:		#X venceu
+            return 1
+        elif    :		#Empate
+            return 0
+        else:			#Jogo não acabou
+        	return -1 	
 
     def limpa_jogadas(self): #Limpa jogadas (renicia o jogo)
