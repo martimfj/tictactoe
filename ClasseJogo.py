c5e69eae9415775a6ad749bc44bb85a91e1e4b6a
raphacosta27 <rapha2706@hotmail.com>
2016-04-20 07:53:09 -0300

    Botoes gerados manualmente e nao  mais com o for. Botoes aparecendo a letra quando apertados e tambem param de rodar quando apertados.

--- ClasseJogo.py
+++ ClasseJogo.py
@@ -1,6 +1,5 @@
 
 
-<<<<<<< HEAD
 class Jogo:  
     def __init__(self):
         
@@ -20,34 +19,37 @@ class Jogo:
         else:
             self.jogador = 1
 
-#def verifica_ganhador(self): #Retorna 0 em empate, 1 se X vencer, 2 se O vencer, -1 caso contrário.
-     
-=======
-#receber jogada por botao, verificar se jogo terminou, 
-#alterar jogadores
-    
-class Jogo:
-    def __init__(self):
-        self.tabuleiro = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-        self.jogador = 1 #1 = Jogador X
-
-    def recebe_jogada(self, linha, coluna): #receber e registrar a jogada, deve alterar os jogadores e não retorna nada.
-        self.tabuleiro[linha][coluna] = self.jogador
-        if self.jogador == 1:
-        	self.jogador = 2
-        else: 
-        	self.jogador = 1
-    
     def verifica_ganhador(self): #Retorna 0 em empate, 1 se X vencer, 2 se O vencer, -1 caso contrário.
-    	for 
-    	if self.tabuleiro: 		#O venceu
-            return 2 
-        elif  	:		#X venceu
-            return 1
-        elif    :		#Empate
-            return 0
-        else:			#Jogo não acabou
-        	return -1 	
->>>>>>> d5e67ae9ae46c8e2d96a5ccac49180f1859a413b
-
+#        if self.tab[0][0] + self.tab[0][1] + self.tab[0][2] == 3:
+#            return 1 
+#        elif self.tab[1][0] + self.tab[1][1] + self.tab[1][2] == 3: 
+#           return 1
+#        elif self.tab[2][0] + self.tab[2][1] + self.tab[2][2] == 3: 
+       self.somalinha1 = 0 
+       for j in range (0,3):
+           self.somalinha1 += self.tab[0][j] 
+           if self.somalinha1 == 3:
+               return 1 
+           elif self.somalinha1 == 6:
+               return 2 
+           
+           
+       self.somalinha2 = 0 
+       for j in range (0,3):
+           self.somalinha2 += self.tab[1][j] 
+           if self.somalinha2 == 3:
+               return 1 
+           elif self.somalinha2 == 6:
+               return 2 
+           
+           
+       self.somalinha3 = 0 
+       for j in range (0,3):
+           self.somalinha3 += self.tab[2][j]
+           if self.somalinha3 == 3:
+               return 1 
+           elif self.somalinha3 == 6:
+               return 2 
+           
+       print(self.somalinha1, self.somalinha2, self.somalinha3)
 #def limpa_jogadas(self):
